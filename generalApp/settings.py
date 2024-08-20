263270aacec9a3d8102553b7ac385a684a4b7209
Blockblocked <leduan12044@gmail.com>
2024-08-20 16:06:40 -0500

    Internal server error 500

--- generalApp/settings.py
+++ generalApp/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 import os
 from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
